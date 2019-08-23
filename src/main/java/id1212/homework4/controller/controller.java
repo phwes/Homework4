327838febfe7f0360bb302fc4d5f73521d2ea527
Philip Wester <Philip.Wester@outlook.com>
2019-08-23 14:20:56 +0200

    Init commit. Payara starts correctly.

---
+++ src/main/java/id1212/homework4/controller/controller.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package id1212.homework4.controller;
+
+import javax.ejb.Stateless;
+
+/**
+ *
+ * @author philip
+ */
+@Stateless
+public class controller {
+
+    // Add business logic below. (Right-click in editor and choose
+    // "Insert Code > Add Business Method")
+}
---
+++ src/main/java/id1212/homework4/view/manBean.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package id1212.homework4.view;
+
+import javax.inject.Named;
+import javax.enterprise.context.Dependent;
+
+/**
+ *
+ * @author philip
+ */
+@Named(value = "manBean")
+@Dependent
+public class manBean {
+
+    /**
+     * Creates a new instance of manBean
+     */
+    public manBean() {
+    }
+    
+}
